e1292697aa92fe186331e7231c405c7f0a98f2b1
jt8845186 <98743057+jt8845186@users.noreply.github.com>
2022-06-04 15:20:40 +0100

    tbgl website

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,64 @@
 import headerStyles from '../styles/Header.module.css'
+import {Fade} from 'react-slideshow-image'
+import 'react-slideshow-image/dist/styles.css'
 
 const Header = () => {
+  const images= [
+    'img/hero.png',
+    'img/2mw.png',
+    'img/90kwatt.jpg',
+    'img/50kw.png',
+  ]
+
+  const slideProperties = {
+    indicators: true,
+    scale: 1.2,
+    duration:5000,
+    transitionDuration:500,
+    arrows: true,
+    infinite:true,
+    pauseonHover: true,
+    onchange:(oldIndex, newIndex) => {
+      console.log(`slide transition from ${oldIndex} to ${newIndex}`)
+    }
+  }
+
   return (
       <>
         <section className={headerStyles.hero} >
-            <h1 className={headerStyles.text}>Let your power problem be our problem.</h1>
-            <div className={headerStyles.description}>Having unstable and high cost of power generation is 
-            not fun.We want to make it a better experience for you.</div>
-            <button className={headerStyles.cta}>LEARN MORE</button>
+        <div className='slide-container'>
+
+        <Fade {...slideProperties}>
+            <div className='each-slide'>
+                <div className={headerStyles.carousel_image} style={{backgroundImage: `url(${images[0]})`}}>
+                  <h1 className={headerStyles.text}>Let your power problem be our problem.</h1>
+                  <div className={headerStyles.description}>Having unstable and high cost of power generation is 
+                  not fun.We want to make it a better experience for you.</div>
+                  <button className={headerStyles.cta}>LEARN MORE</button>
+              </div>
+            </div>
+            <div className='each-slide'>
+              <div  className={headerStyles.carousel_image} style={{backgroundImage: `url(${images[1]})`}}>
+                  <h1 className={headerStyles.text}>2 Mega Watts Hybrid Solar System Installation</h1>
+                  <button className={headerStyles.cta}>LEARN MORE</button>
+              </div>
+            </div>
+            <div className='each-slide'>
+              <div className={headerStyles.carousel_image}  style={{backgroundImage: `url(${images[2]})`}}>
+                  <h1 className={headerStyles.text}>90 Kilo Watts All in one Solar System Installation</h1>
+                  <button className={headerStyles.cta}>LEARN MORE</button>
+              </div>
+            </div>
+            <div className='each-slide'>
+              <div className={headerStyles.carousel_image}  style={{backgroundImage: `url(${images[3]})`}}>
+                  <h1 className={headerStyles.text}>50 Kilo Watts Hybrid Solar System Installation</h1>
+                  <button className={headerStyles.cta}>LEARN MORE</button>
+              </div>
+            </div>
+          </Fade>
+        </div>
         </section>
+
       </>
     
   )
--- components/Testimonial.js
+++ components/Testimonial.js
@@ -1,35 +1,139 @@
 import Image from "next/image"
-import styles from '../styles/Layout.module.css'
+import styles from '../styles/Testimonial.module.css'
+import {Fade} from 'react-slideshow-image'
+import 'react-slideshow-image/dist/styles.css'
 
 const Testimonial = () => {
+
+    const images= [
+      '/img/testimonial.png',
+      '/img/testimonial2.jpg',
+      '/img/testimonial3.jpg',
+    ]
+  
+    const slideProperties = {
+      indicators: true,
+      scale: 1.2,
+      duration:5000,
+      transitionDuration:500,
+      arrows: false,
+      infinite:true,
+      pauseonHover: true,
+      onchange:(oldIndex, newIndex) => {
+        console.log(`slide transition from ${oldIndex} to ${newIndex}`)
+      }
+    }
+    
+    const slide_container_img={
+        borderRadius:'15px',
+    }
+    const each_slide={
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignContent: 'center',
+    }
+
   return (
     <>
-    
     <div className={styles.containerTestimonial}>
-      <h4 style={{color: '#fff'}}>TESTIMONIAL</h4>
-      <h1 style={{color: '#fff'}}>What They Said About Us</h1>
-      <div className={styles.testimonialSlider}>
-        <div className={styles.testimonialDesc}>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-              ut labore et dolore magna aliqua.
-            </h4>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-              ut labore et dolore magna aliqua.
-            </h4>
-            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
-              ut labore et dolore magna aliqua.
-            </h4>
-            <h5>
-              John Doe
-            </h5>
-            <h5>
-              Managing Director, Petrocam Igando.
-            </h5>
-        </div>
-        <div className={styles.testimonialSlideImg}>
-          <Image src='/img/testimonial.png' alt='testimonial' height='557' width='483' />
-        </div>
+      
+        <h4 style={{color: '#fff',textAlign: 'center'}}>TESTIMONIAL</h4>
+        <h1 style={{color: '#fff',textAlign: 'center',marginBottom: '40px'}}>What They Said About Us</h1>
+      <div className="slide-container">
+        <Fade {...slideProperties}>
+              <div className='each-slide' style={each_slide}>
+                  <div className={styles.testimonial_text}>
+                      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.
+                      </h4>
+                      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.
+                      </h4>
+                      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.
+                      </h4>
+                      <h5>
+                        John Doe
+                      </h5>
+                      <h5>
+                        Managing Director, Petrocam Igando.
+                      </h5>
+                  </div>
+                  <div>
+                    <Image style={slide_container_img} src={images[0]} alt='testimonial' height='557' width='483' />
+                  </div>
+              </div>
+              <div className='each-slide' style={each_slide}>
+                  <div className={styles.testimonial_text}>
+                      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.
+                      </h4>
+                      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.
+                      </h4>
+                      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.
+                      </h4>
+                      <h5>
+                        John Doe
+                      </h5>
+                      <h5>
+                        General Manager, NNPC Abeokuta.
+                      </h5>
+                  </div>
+                  <div>
+                    <Image style={slide_container_img} src={images[1]} alt='testimonial' height='557' width='483' />
+                  </div>
+              </div>
+              <div className='each-slide' style={each_slide}>
+                  <div className={styles.testimonial_text}>
+                      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.
+                      </h4>
+                      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.
+                      </h4>
+                      <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                        ut labore et dolore magna aliqua.
+                      </h4>
+                      <h5>
+                        John Doe
+                      </h5>
+                      <h5>
+                        Founder, OG Farms Igando.
+                      </h5>
+                  </div>
+                  <div>
+                    <Image style={slide_container_img} src={images[2]} alt='testimonial' height='557' width='483' />
+                  </div>
+              </div>
+        </Fade>
       </div>
+        {/* <h4 style={{color: '#fff'}}>TESTIMONIAL</h4>
+        <h1 style={{color: '#fff'}}>What They Said About Us</h1>
+        <div className={styles.testimonialSlider}>
+          <div >
+              <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua.
+              </h4>
+              <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua.
+              </h4>
+              <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                ut labore et dolore magna aliqua.
+              </h4>
+              <h5>
+                John Doe
+              </h5>
+              <h5>
+                Managing Director, Petrocam Igando.
+              </h5>
+          </div>
+          <div className={styles.testimonialSlideImg}>
+            <Image src='/img/testimonial.png' alt='testimonial' height='557' width='483' />
+          </div>
+        </div> */}
     </div>
     </>
   )
